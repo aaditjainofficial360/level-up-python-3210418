29a9408f306d13814fbbb595e284be5fd2b3c831
Aadit Jain <105768739+aaditjainofficial360@users.noreply.github.com>
2023-08-11 01:24:51 +0530

    Submitted

---
+++ My Code Challenges/Prime_factors.py
@@ -0,0 +1,19 @@
+def get_prime_factors(number):
+    factors = []
+    prime_factors = []
+    flag = True
+    for i in range(1, number+1):
+        if number % i == 0:
+            factors.append(i)
+    for j in factors[1:]:
+        for k in range(2, j):
+            if j % k == 0:
+                flag = False
+        if flag:
+            prime_factors.append(j)
+    return prime_factors
+
+
+print(get_prime_factors(14))
+print(get_prime_factors(45))
+print(get_prime_factors(67))
