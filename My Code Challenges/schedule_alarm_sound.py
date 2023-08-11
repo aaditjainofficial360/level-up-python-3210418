8db5aaf32b73465a7d5c7d7377bddae99b6cfef9
aaditjainofficial360 <105768739+aaditjainofficial360@users.noreply.github.com>
2023-08-11 14:19:54 +0530

    Update schedule_alarm_sound.py

--- My Code Challenges/schedule_alarm_sound.py
+++ My Code Challenges/schedule_alarm_sound.py
@@ -4,8 +4,9 @@ def schedule_function(event_time,function,message):
     start_time=time.time()
     while start_time<=event_time:
         function(message)
+        start_time+=1
 
     return 'Done'
 
 # Event_time(in seconds) : 500 seconds
-schedule_function(time.time()+0.008,print,'Hey Brother!')
+schedule_function(time.time()+20,print,'Hey Brother!')
